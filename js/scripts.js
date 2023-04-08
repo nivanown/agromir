618cdb4bf98c646538812892cfa1f9c4767108e4
Nikita <32540777+nivanown@users.noreply.github.com>
2023-04-08 19:58:36 +0500

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -184,3 +184,26 @@ if (document.getElementById('map-2')) {
     }
   });
 }   
+
+/*- promo-code -*/
+if ($("#promo-code")) {
+  // функция возвращает cookie с именем name, если есть, если нет, то undefined
+  function getCookie(name) {
+    var matches = document.cookie.match(new RegExp("(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+  }
+  // проверяем, есть ли у нас cookie, с которой мы не показываем окно и если нет, запускаем показ
+  var alertwin = getCookie("alertwin");
+  if (alertwin != "no") {
+    $(document).mouseleave(function (e) {
+      if (e.clientY < 10) {
+        myModal.open('#promo-code');
+        // записываем cookie на 1 день, с которой мы не показываем окно
+        var date = new Date;
+        date.setDate(date.getDate() + 1);
+        document.cookie = "alertwin=no; path=/; expires=" + date.toUTCString();
+      }
+    });
+  }
+}
